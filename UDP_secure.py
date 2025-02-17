e9f8a2be8cccea8eb52b615fe8437c3a1618cc6f
Gabriela Singulani <gabriela.singulani@ice.ufjf.br>
2025-02-17 10:48:25 -0300

    Cria classe para cliente

--- UDP_secure.py
+++ UDP_secure.py
@@ -10,13 +10,9 @@ class UDP_secure:
         self.buffer = buff
         self.protocol = socket.SOCK_DGRAM
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.timer = None
-        self.maxTimer = 100
 
     def send(self, ip, port, data):
         self.socket.sendto(data, (ip, port))
-        if(self.timer == None):
-            self.timer = time.time()
     
     def receive(self, from_ip):
         self.socket.bind((from_ip, self.port))
@@ -25,6 +21,21 @@ class UDP_secure:
             print("Received:", data, "from", address)
             self.send(address[0], address[1], b"Recebido!")
 
+    def __del__(self):
+        self.socket.close()
+
+class Client(UDP_secure):
+    def __init__(self, ip, port, buff):
+        super().__init__(ip, port, buff)
+        self.timer = None
+        self.maxTimer = 1
+
+    def send(self, ip, port, data):
+        super().send(ip, port, data)
+        if(self.timer == None):
+            self.timer = time.time()
+        self.waitAck()
+
     def waitAck(self):
         while time.time() - self.timer < self.maxTimer:
             data, address = self.socket.recvfrom(self.port)
@@ -32,4 +43,4 @@ class UDP_secure:
             break
 
     def __del__(self):
-        self.socket.close()
+        super().__del__()
--- sender.py
+++ sender.py
@@ -1,6 +1,6 @@
 import UDP_secure
 from config import *
 
-sender = UDP_secure.UDP_secure(IP_sender, port_sender, buffer_sender)
+sender = UDP_secure.Client(IP_sender, port_sender, buffer_sender)
 sender.send(IP_receiver, port_receiver, b"Hello, World!")
-sender.waitAck()
+# sender.waitAck()
