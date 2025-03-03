79404d31c3193a451dc3ed393a00c07ee4c0f7c2
JVCA101 <joaovictor.correa@estudante.ufjf.com>
2025-03-03 16:02:20 -0300

    isNotInWindow reescrito para mais fácil entendimento

--- UDP_secure.py
+++ UDP_secure.py
@@ -30,8 +30,18 @@ class UDP_secure:
         return str(index) + ":" + "Recebido!"
 
     def isNotInWindow(self, index):
-        return index >= (self.windowStart + self.windowSize)%self.sequenceSize and ((index >= (self.windowStart + self.windowSize)%self.sequenceSize and (self.windowStart + self.windowSize)%self.sequenceSize > self.windowStart) or (index < self.windowStart and (self.windowStart + self.windowSize)%self.sequenceSize < self.windowStart))
+        wndSize = self.windowSize   # get window size
+        wndStart = self.windowStart   # get window start
+        wndEnd = (wndStart + wndSize)%self.sequenceSize   # calculate window end
 
+        afterWndStart  = wndEnd > wndStart and (index >= wndEnd or  index < wndStart)  # index is after the window end and window end is not at the beginning of the sequence
+        beforeWndStart = wndEnd < wndStart and (index >= wndEnd and index < wndStart)  # index is before the window start and window end is at the beginning of the sequence
+
+        # if index is after the window end and window end is not at the beginning of the sequence or
+        # if index is before the window start and window end is at the beginning of the sequence, 
+        #     return True
+        return afterWndStart or beforeWndStart
+    
     def moveWindow(self):
         i = self.windowStart
 
