9bdb2773442ea435920b490da6721bc69c9e9c92
quarerma <gabriel.oliveira.quaresma@gmail.com>
2025-03-15 16:25:50 -0300

    feat: add ack control + add Fast Retransmit + SlowStart + Congestion Avoidance

--- Sender.py
+++ Sender.py
@@ -6,7 +6,6 @@ import asyncio
 from UDPSecure import UDPSecure
 
 
-
 #? --------------Sender Class--------------
 class Sender(UDPSecure):
     def __init__(self, ip, port, buff):
@@ -17,6 +16,12 @@ class Sender(UDPSecure):
         self.timer = None
         self.maxTimer = 1
         self.currentIndex = 0
+        
+        # Congestion Control Variables
+        self.cwnd = 1  # Start with 1 packet
+        self.ssthresh = 8  # Initial slow start threshold (adjustable)
+        self.dup_ack_count = 0
+        self.last_ack = -1  # Track last acknowledged packet
 
     def connect(self, ip, port):
         super().send(ip, port, b"SYN")
@@ -38,34 +43,76 @@ class Sender(UDPSecure):
         self.__del__()
 
     def send(self, ip, port, data):
+        if self.cwnd <= 0:
+            return  # Avoid sending if congestion window is 0
+        
         data = (str(self.currentIndex) + ":").encode() + data
         super().send(ip, port, data)
-        # if self.timer == None:
         self.timer = time.time()
-        error = self.waitAck()
+        error, ack = self.waitAck()
+        
         if error:
-            return
+            self.handle_loss()
+        else:
+            self.handle_ack(ack)
+        
         self.currentIndex = (self.currentIndex + 1) % self.sequenceSize
 
-    # def waitAck(self):
-    #     while time.time() - self.timer < self.maxTimer:
-    #         data, address = super().receive()
-    #         message = (data.decode()).split(":")[0]
-    #         if message == "Erro":
-    #             print(data.decode())
-    #             return True
-    #         sequenceNum = int(message)
-    #         if sequenceNum == self.windowStart:
-    #             self.timer = time.time()
-    #         self.markPkt(sequenceNum)
-    #         return False
-    #     return True
+    def waitAck(self):
+        """Wait for an ACK and check for duplicate ACKs."""
+        while time.time() - self.timer < self.maxTimer:
+            data, address = super().receive()
+            message = data.decode().split(":")[0]
+            
+            if message == "Erro":
+                print(data.decode())
+                return True, None
+            
+            sequenceNum = int(message)
+            if sequenceNum == self.last_ack:
+                self.dup_ack_count += 1
+                if self.dup_ack_count == 3:
+                    self.handle_fast_retransmit()
+            else:
+                self.dup_ack_count = 0
+                self.last_ack = sequenceNum
+            
+            return False, sequenceNum
+        
+        return True, None
+
+    def handle_loss(self):
+        """Handles congestion event (packet loss)."""
+        print("Packet loss detected! Reducing congestion window.")
+        self.ssthresh = max(self.cwnd // 2, 1)
+        self.cwnd = 1  # Reset to slow start
+        self.dup_ack_count = 0
+
+    def handle_fast_retransmit(self):
+        """Handles Fast Retransmit when three duplicate ACKs are received."""
+        print("Fast retransmit triggered! Halving congestion window.")
+        self.ssthresh = max(self.cwnd // 2, 1)
+        self.cwnd = self.ssthresh  # Reduce cwnd but avoid slow start
+        self.dup_ack_count = 0  # Reset duplicate ACK counter
+
+    def handle_ack(self, ack):
+        """Handles successful ACK reception and updates congestion control."""
+        if self.cwnd < self.ssthresh:
+            # Slow Start Phase: Exponential Growth
+            self.cwnd *= 2
+        else:
+            # Congestion Avoidance Phase: Linear Growth
+            self.cwnd += 1
+        
+        self.dup_ack_count = 0  # Reset duplicate ACK counter
+        print(f"Updated cwnd: {self.cwnd}, ssthresh: {self.ssthresh}")
 
     def markPkt(self, index):
         if self.isNotInWindow(index):
-            return "Erro: Pacote fora da janela " + str(self.windowStart) + " a " + str((self.windowStart + self.windowSize-1)%self.sequenceSize)
+            return "Erro: Pacote fora da janela " + str(self.windowStart) + " a " + str((self.windowStart + self.windowSize-1) % self.sequenceSize)
         self.moveWindow(index)
         return str(index) + ": Recebido!"
 
     def moveWindow(self, index):
         self.windowStart = (index + 1) % self.sequenceSize
+
